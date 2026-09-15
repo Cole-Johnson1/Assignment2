8a7054871df5448ccb10752ba7cadcf1706a8563
Cole <cjohnson8194@conestogac.on.ca>
2026-09-15 13:58:07 -0400

    Step 3: initial source code

--- source.cpp
+++ source.cpp
@@ -1,10 +1,50 @@
 #include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <vector>
 
 using namespace std;
 
+struct STUDENT_DATA {
+    string firstName;
+    string lastName;
+};
+
 int main(){
 
-    cout << "Hello World";
+    ifstream inFile("StudentData.txt");
+    if (!inFile) {
+        // in case the exe is run from the output/ folder
+        inFile.open("../StudentData.txt");
+    }
+    if (!inFile) {
+        cerr << "Error: could not open StudentData.txt" << endl;
+        return 1;
+    }
 
-    return 1;
+    vector<STUDENT_DATA> students;
+    string line;
+
+    while (getline(inFile, line)) {
+        if (line.empty()) {
+            continue;
+        }
+
+        stringstream ss(line);
+        STUDENT_DATA student;
+
+        getline(ss, student.firstName, ',');
+        getline(ss, student.lastName);
+
+        students.push_back(student);
+    }
+
+    inFile.close();
+
+    for (const STUDENT_DATA& student : students) {
+        cout << student.firstName << " " << student.lastName << endl;
+    }
+
+    return 0;
 }
